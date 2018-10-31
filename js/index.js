a43ac2cb4f5dfdec3d6387d307fe3838d9938e32
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-10-31 12:48:04 +0530

    [issue #10] search result on google acc to filetype completed

--- js/index.js
+++ js/index.js
@@ -1,38 +1,56 @@
-$(document).ready(function() {
-    $('#softwares').on('click', function() {
-        $('#filetype').text('Softwares');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Wondershare Filmora )');
-    });
-    $('#games').on('click', function() {
-        $('#filetype').text('PC / Mobile Games');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Max Payne )');
-    });
-    $('#series').on('click', function() {
-        $('#filetype').text('TV Series');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : The Flash S01 )');
-    });
-    $('#movies').on('click', function() {
-        $('#filetype').text('Movies');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : The Avengers )');
-    });
-    $('#videos').on('click', function() {
-        $('#filetype').text('Videos');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Funny Cat )');
-    });
-    $('#books').on('click', function() {
-        $('#filetype').text('Books');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Revolution 2020 )');
-    });
-    $('#images').on('click', function() {
-        $('#filetype').text('Images');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Cat )');
-    });
-    $('#songs').on('click', function() {
-        $('#filetype').text('Songs');
-        $('#inputBox').attr('placeholder', 'Start typing... ( eg. : Shape of you )');
-    });
-    $('#others').on('click', function() {
-        $('#filetype').text('Others');
-        $('#inputBox').attr('placeholder', 'Start typing...');
-    });
-});
+
+function setFiletype(fileType, buttonData, icon){
+    var defaultString = 'Start typing...';
+    if (fileType==-1) {
+        fileType="";
+    }
+    switch(icon) {
+        case 'windows':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Wondershare Filmora )');
+            break;
+        case 'tv':
+            $("#query").prop('placeholder',defaultString+' ( eg. : The Flash S01 )');
+            break;
+        case 'film':
+            $("#query").prop('placeholder',defaultString+' ( eg. : The Avengers )');
+            break;
+        case 'youtube':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Funny Cat )');
+            break;
+        case 'music':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Shape of you )');
+            break;
+        case 'book':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Revolution 2020 )');
+            break;
+        case 'gamepad':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Max Payne )');
+            break;
+        case 'camera-retro':
+            $("#query").prop('placeholder',defaultString+' ( eg. : Cat )');
+            break;
+        case 'asterisk':
+            $("#query").prop('placeholder',defaultString);
+            break;
+    }
+    $("#fileType").prop('value',fileType);
+    $("#ddbutton").html('<i class="fa fa-'+icon+' fa-spin" aria-hidden="true"></i> '+buttonData+' <span class="caret"></span>');
+    $("#query").focus();
+}
+
+function searchGoogle () {
+    var query = document.getElementById('query').value;
+    var fileType = document.getElementById('fileType').value;
+    if (fileType=="") {
+        var finalQuery = query+" -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml) intitle:index.of -inurl:(listen77|mp3raid|mp3toss|mp3drug|index_of|wallywashis)";
+    } else {
+        var finalQuery = query+" +("+fileType+") -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml) intitle:index.of -inurl:(listen77|mp3raid|mp3toss|mp3drug|index_of|wallywashis)";
+    }
+    var url = "https://www.google.com/search?q="+encodeURIComponent(finalQuery);
+    // console.log(url);
+    window.open(url, '_blank');
+}
+
+function openDropdown(){
+    $('#ddbutton').click();
+}
